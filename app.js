c7f0fcb079a64fc30eb4eecdc03caedc89af3bef
Gavin <gavinberetvas@ucsb.edu>
2023-11-15 17:09:03 -0800

    styling

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-document
-  .querySelector(".create-account")
-  .addEventListener("click", function (event) {
-    if (!checkPasswordsMatch()) {
-      document.getElementById("passwordtest").innerHTML =
-        "passwords must match";
-      document.getElementById("password").style.borderColor = "#981717";
-      document.getElementById("password2").style.borderColor = "#981717";
-      document.getElementById("password").style.borderRadius = "4px";
-      document.getElementById("password2").style.borderRadius = "4px";
-      document.getElementById("password").style.transitionProperty = "border";
-      document.getElementById("password2").style.transitionProperty = "border";
-      document.getElementById("password").style.transitionDuration = ".5s";
-      document.getElementById("password2").style.transitionDuration = ".5s";
-      event.preventDefault();
-    } else {
-      document.getElementById("passwordtest").innerHTML = "";
-    }
-  });
-
 function checkPasswordsMatch() {
-  const password1 = document.getElementById("password").value;
-  const password2 = document.getElementById("password2").value;
-
+  const password1 = document.getElementById('password').value;
+  const password2 = document.getElementById('password2').value;
   if (password1 === password2) {
     // Passwords match
     return true;
-  } else {
-    // Passwords do not match
-    return false;
   }
+  // Passwords do not match
+  return false;
 }
+
+document.querySelector('.create-account').addEventListener('click', (event) => {
+  if (!checkPasswordsMatch()) {
+    document.getElementById('passwordtest').innerHTML = 'passwords must match';
+    document.getElementById('password').style.borderColor = '#981717';
+    document.getElementById('password2').style.borderColor = '#981717';
+    document.getElementById('password').style.borderRadius = '4px';
+    document.getElementById('password2').style.borderRadius = '4px';
+    document.getElementById('password').style.transitionProperty = 'border';
+    document.getElementById('password2').style.transitionProperty = 'border';
+    document.getElementById('password').style.transitionDuration = '.5s';
+    document.getElementById('password2').style.transitionDuration = '.5s';
+    event.preventDefault();
+  } else {
+    document.getElementById('passwordtest').innerHTML = '';
+  }
+});
